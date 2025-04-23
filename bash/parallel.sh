76512467a372b0e8d1572be2b276bce5bf7e632c
Глазов Николай Олегович <nglazov@cspfmba.ru>
2025-04-23 10:52:20 +0300

    updated parallel.sh

--- bash/parallel.sh
+++ bash/parallel.sh
@@ -1,1 +1,10 @@
+
+ls -1 .. | parallel echo {}
+
+parallel -j 1 echo {1}_{2} ::: A B C D E ::: 1 2 3 4 5 6 7
+
+seq 2007 2019 > years.txt && seq 1 12 > months.txt
+parallel -j 1 --eta --progress --verbose echo {1} {2} :::: years.txt :::: months.txt
+rm -f years.txt months.txt
+
 seq 100 | parallel --jobs 0 --eta --progress --bar 'sleep 0.1'
